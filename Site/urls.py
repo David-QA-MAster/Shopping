f8ebb5f7093bc863b4297300c1b6b625ecaaa498
David Oliveira <david12ol.100@gmail.com>
2023-05-22 22:25:13 -0300

    add

--- Site/urls.py
+++ Site/urls.py
@@ -3,6 +3,9 @@ from Site import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('sobre-a-empresa', views.institucional, name='institucional'),
+    path('cadastro', views.cadastro_cliente, name='cadastro_cliente'),
+    path('contato', views.contato_cliente, name='contato_cliente'),
     path('produtos', views.produto_lista, name='produto_lista'),
     path('produto', views.produto_lista, name='produto_detalhe'),
 
