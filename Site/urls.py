73c5f36f68587269d2d127f80a2e6bf32afe15c4
David Oliveira <david12ol.100@gmail.com>
2023-05-24 22:28:51 -0300

    add produtos detalhes

--- Site/urls.py
+++ Site/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path('cadastro', views.cadastro_cliente, name='cadastro_cliente'),
     path('contato', views.contato_cliente, name='contato_cliente'),
     path('produtos', views.produto_lista, name='produto_lista'),
-    path('produto', views.produto_lista, name='produto_detalhe'),
+    path('produtos/<int:id>', views.produto_lista_por_id, name= 'produto_lista_por_id'),
+    path('produto/<int:id>', views.produto_detalhe, name='produto_detalhe'),
 
 
 ]
